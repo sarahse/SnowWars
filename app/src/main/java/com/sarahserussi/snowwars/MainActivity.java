d10b22cf399248126c8b0ff3dd5482bf65b4ed89
sarahse <sarahse@stud.ntnu.no>
2015-03-17 12:03:19 +0100

    gameloop thread

--- app/src/main/java/com/sarahserussi/snowwars/MainActivity.java
+++ app/src/main/java/com/sarahserussi/snowwars/MainActivity.java
@@ -1,12 +1,13 @@
 package com.sarahserussi.snowwars;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private GameState gameState;
 
