f68410f9fb50cf7574dbcb282c0e0533b860e482
sarahse <sarahse@stud.ntnu.no>
2015-04-09 11:36:27 +0200

    multitouch

--- app/src/main/java/com/sarahserussi/snowwars/GameState.java
+++ app/src/main/java/com/sarahserussi/snowwars/GameState.java
@@ -196,34 +196,8 @@ public class GameState extends SurfaceView implements SurfaceHolder.Callback {
         }
 
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            player1.handleActionDown((int) event.getX(), (int) event.getY());
-            if (player1.isTouched()) {
-                player1.setPositionX((int) event.getX());
-                /* checks if the player touches the net */
-                if (player1.getSpriteRect().intersect(getSpriteLineRect())) {
-                    player1.setPositionX((getScreenWidth() / 2 - player1.getSpriteRect().width() - (verticalLine.getWidth() / 2)));
-                }
-            }
-
-            player2.handleActionDown((int) event.getX(), (int) event.getY());
-            if (player2.isTouched()) {
-                player2.setPositionX((int) event.getX());
-                /* checks if the player touches the net */
-                if (player2.getSpriteRect().intersect(getSpriteLineRect())) {
-                    player2.setPositionX((getScreenWidth() / 2) + (verticalLine.getWidth() / 2));
-                }
-                /* checks if the player goes out of the screen */
-                if ((player2.getPositionX() + player2.getSpriteRect().width()) >= getScreenWidth()) {
-                    player2.setPositionX(getScreenWidth() - player2.getSpriteRect().width());
-                }
-            }
-        }
-
-
-
-            if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                if (event.getAction() == MotionEvent.ACTION_POINTER_DOWN){
-                player1.handleActionPointerDown((int) event.getX(), (int) event.getY());
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                player1.handleActionDown((int) event.getX(), (int) event.getY());
                 if (player1.isTouched()) {
                     player1.setPositionX((int) event.getX());
                 /* checks if the player touches the net */
@@ -232,6 +206,28 @@ public class GameState extends SurfaceView implements SurfaceHolder.Callback {
                     }
                 }
 
+                player2.handleActionDown((int) event.getX(), (int) event.getY());
+                if (player2.isTouched()) {
+                    player2.setPositionX((int) event.getX());
+                /* checks if the player touches the net */
+                    if (player2.getSpriteRect().intersect(getSpriteLineRect())) {
+                        player2.setPositionX((getScreenWidth() / 2) + (verticalLine.getWidth() / 2));
+                    }
+                /* checks if the player goes out of the screen */
+                    if ((player2.getPositionX() + player2.getSpriteRect().width()) >= getScreenWidth()) {
+                        player2.setPositionX(getScreenWidth() - player2.getSpriteRect().width());
+                    }
+                }
+            }
+            if (event.getAction() == MotionEvent.ACTION_POINTER_DOWN){
+                player1.handleActionPointerDown((int) event.getX(), (int) event.getY());
+                if (player1.isTouched()) {
+                    player1.setPositionX((int) event.getX());
+                /* checks if the player touches the net */
+                    if (player1.getSpriteRect().intersect(getSpriteLineRect())) {
+                        player1.setPositionX((getScreenWidth() / 2 - player1.getSpriteRect().width() - (verticalLine.getWidth() / 2)));
+                    }
+                }
 
                 player2.handleActionPointerDown((int) event.getX(), (int) event.getY());
                 if (player2.isTouched()) {
@@ -247,6 +243,7 @@ public class GameState extends SurfaceView implements SurfaceHolder.Callback {
                 }
             }
         }
+
         return true;
     }
 
