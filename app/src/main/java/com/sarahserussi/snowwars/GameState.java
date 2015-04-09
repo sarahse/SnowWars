4486cb226633809a8da5afb5885ae4bda4c1cd2e
sarahse <sarahse@stud.ntnu.no>
2015-04-09 11:24:43 +0200

    multitouch

--- app/src/main/java/com/sarahserussi/snowwars/GameState.java
+++ app/src/main/java/com/sarahserussi/snowwars/GameState.java
@@ -194,7 +194,7 @@ public class GameState extends SurfaceView implements SurfaceHolder.Callback {
                 player2.setTouched(false);
             }
         }
-        if (event.getAction() == MotionEvent.ACTION_DOWN){
+
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
             player1.handleActionDown((int) event.getX(), (int) event.getY());
             if (player1.isTouched()) {
@@ -218,7 +218,7 @@ public class GameState extends SurfaceView implements SurfaceHolder.Callback {
                 }
             }
         }
-        }
+
 
         if (event.getAction() == MotionEvent.ACTION_POINTER_DOWN){
             if (event.getAction() == MotionEvent.ACTION_MOVE) {
