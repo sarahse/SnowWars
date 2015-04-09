67c31bb68a3af84be5c11672e7323d53345bccc2
sarahse <sarahse@stud.ntnu.no>
2015-04-09 11:21:55 +0200

    multitouch

--- app/src/main/java/com/sarahserussi/snowwars/GameState.java
+++ app/src/main/java/com/sarahserussi/snowwars/GameState.java
@@ -194,14 +194,14 @@ public class GameState extends SurfaceView implements SurfaceHolder.Callback {
                 player2.setTouched(false);
             }
         }
-
         if (event.getAction() == MotionEvent.ACTION_DOWN){
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
             player1.handleActionDown((int) event.getX(), (int) event.getY());
-            if (player1.isTouched() ) {
+            if (player1.isTouched()) {
                 player1.setPositionX((int) event.getX());
                 /* checks if the player touches the net */
-                if (player1.getSpriteRect().intersect(getSpriteLineRect())){
-                    player1.setPositionX((getScreenWidth()/2-player1.getSpriteRect().width()-(verticalLine.getWidth()/2)));
+                if (player1.getSpriteRect().intersect(getSpriteLineRect())) {
+                    player1.setPositionX((getScreenWidth() / 2 - player1.getSpriteRect().width() - (verticalLine.getWidth() / 2)));
                 }
             }
 
@@ -209,36 +209,40 @@ public class GameState extends SurfaceView implements SurfaceHolder.Callback {
             if (player2.isTouched()) {
                 player2.setPositionX((int) event.getX());
                 /* checks if the player touches the net */
-                if (player2.getSpriteRect().intersect(getSpriteLineRect())){
-                    player2.setPositionX((getScreenWidth()/2)+(verticalLine.getWidth()/2));
+                if (player2.getSpriteRect().intersect(getSpriteLineRect())) {
+                    player2.setPositionX((getScreenWidth() / 2) + (verticalLine.getWidth() / 2));
                 }
                 /* checks if the player goes out of the screen */
-                if ((player2.getPositionX()+player2.getSpriteRect().width()) >= getScreenWidth()){
-                    player2.setPositionX(getScreenWidth()-player2.getSpriteRect().width());
+                if ((player2.getPositionX() + player2.getSpriteRect().width()) >= getScreenWidth()) {
+                    player2.setPositionX(getScreenWidth() - player2.getSpriteRect().width());
                 }
             }
         }
+        }
 
         if (event.getAction() == MotionEvent.ACTION_POINTER_DOWN){
-            player1.handleActionPointerDown((int) event.getX(), (int) event.getY());
-            if (player1.isTouched() ) {
-                player1.setPositionX((int) event.getX());
+            if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                player1.handleActionPointerDown((int) event.getX(), (int) event.getY());
+                if (player1.isTouched()) {
+                    player1.setPositionX((int) event.getX());
                 /* checks if the player touches the net */
-                if (player1.getSpriteRect().intersect(getSpriteLineRect())){
-                    player1.setPositionX((getScreenWidth()/2-player1.getSpriteRect().width()-(verticalLine.getWidth()/2)));
+                    if (player1.getSpriteRect().intersect(getSpriteLineRect())) {
+                        player1.setPositionX((getScreenWidth() / 2 - player1.getSpriteRect().width() - (verticalLine.getWidth() / 2)));
+                    }
                 }
-            }
 
-            player2.handleActionPointerDown((int) event.getX(), (int) event.getY());
-            if (player2.isTouched()) {
-                player2.setPositionX((int) event.getX());
+
+                player2.handleActionPointerDown((int) event.getX(), (int) event.getY());
+                if (player2.isTouched()) {
+                    player2.setPositionX((int) event.getX());
                 /* checks if the player touches the net */
-                if (player2.getSpriteRect().intersect(getSpriteLineRect())){
-                    player2.setPositionX((getScreenWidth()/2)+(verticalLine.getWidth()/2));
-                }
+                    if (player2.getSpriteRect().intersect(getSpriteLineRect())) {
+                        player2.setPositionX((getScreenWidth() / 2) + (verticalLine.getWidth() / 2));
+                    }
                 /* checks if the player goes out of the screen */
-                if ((player2.getPositionX()+player2.getSpriteRect().width()) >= getScreenWidth()){
-                    player2.setPositionX(getScreenWidth()-player2.getSpriteRect().width());
+                    if ((player2.getPositionX() + player2.getSpriteRect().width()) >= getScreenWidth()) {
+                        player2.setPositionX(getScreenWidth() - player2.getSpriteRect().width());
+                    }
                 }
             }
         }
